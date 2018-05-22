11b4ee73fe4a45bb3640f334caa09ef65a1f0439
Albert Alises <albert.alises@gmail.com>
2018-05-22 10:48:45 +0200

    Fixed puppeteer default import

--- src/components/order-burrito-form.tsx
+++ src/components/order-burrito-form.tsx
@@ -5,11 +5,11 @@ import { orderBurritoFormModel } from "../models/order-burrito-model";
 import connectStore from "../connect"
 import { addOrder } from "../actions"
 
-@connectStore({addOrder})
+@connectStore({ addOrder })
 export default class OrderBurritoForm extends ComponentForm<any, orderBurritoFormModel> {
 
   sizes = ['', 'Small', 'Medium', 'Large'];
-  types = ['', 'Chicken', 'Chilli', 'Vegan','Cheese'];
+  types = ['', 'Chicken', 'Chilli', 'Vegan', 'Cheese'];
 
   state = {
     form: {
@@ -39,73 +39,73 @@ export default class OrderBurritoForm extends ComponentForm<any, orderBurritoFor
   render() {
     return (
       <div>
-         <FormGroup watch={change => this.handleFormChange(change)} preload={this.state.form}>
-        <div class="hero-body">
-                    <h3 class="title has-text-grey">Order Burrito Form</h3>
-                    <p class="subtitle has-text-grey has-text-centered">Please order your burrito here.</p>
-                    <div class="box">
-                    <div class="field is-horizontal">
-                    <div class="field-label">
-                      <label class="label">Type</label>
-                    </div>
-                    <div class="field-body">
-                      <div class="field is-expanded">
-                        <div class="control">
-                          <div class="select is-fullwidth">
-                          <select data-bind="type">
-                            {this.types.map(el => (
-                              <option value={el}>{el}</option>
-                            ))
-                            }
-                          </select>
-                          </div>
-                          <p class="help is-danger">This field is mandatory</p>
-                        </div>
+        <FormGroup watch={change => this.handleFormChange(change)} preload={this.state.form}>
+          <div class="hero-body">
+            <h3 class="title has-text-grey">Order Burrito Form</h3>
+            <p class="subtitle has-text-grey has-text-centered">Please order your burrito here.</p>
+            <div class="box">
+              <div class="field is-horizontal">
+                <div class="field-label">
+                  <label class="label">Type</label>
+                </div>
+                <div class="field-body">
+                  <div class="field is-expanded">
+                    <div class="control">
+                      <div class="select is-fullwidth">
+                        <select data-bind="type">
+                          {this.types.map(el => (
+                            <option value={el}>{el}</option>
+                          ))
+                          }
+                        </select>
                       </div>
+                      <p class="help is-danger">This field is mandatory</p>
                     </div>
                   </div>
-                  <div class="field is-horizontal">
-                    <div class="field-label is-normal">
-                      <label class="label">Size</label>
-                    </div>
-                    <div class="field-body">
-                      <div class="field is-expanded">
-                        <div class="control">
-                          <div class="select is-fullwidth">
-                          <select data-bind="size">
-                            {this.sizes.map(el => (
-                              <option value={el}>{el}</option>
-                            ))
-                            }
-                          </select>
-                          </div>
-                          <p class="help is-danger">This field is mandatory</p>
-                        </div>
+                </div>
+              </div>
+              <div class="field is-horizontal">
+                <div class="field-label is-normal">
+                  <label class="label">Size</label>
+                </div>
+                <div class="field-body">
+                  <div class="field is-expanded">
+                    <div class="control">
+                      <div class="select is-fullwidth">
+                        <select data-bind="size">
+                          {this.sizes.map(el => (
+                            <option value={el}>{el}</option>
+                          ))
+                          }
+                        </select>
                       </div>
+                      <p class="help is-danger">This field is mandatory</p>
                     </div>
                   </div>
-                  <div class="field is-horizontal">
-                  <label class="label">
-                    To take away?
+                </div>
+              </div>
+              <div class="field is-horizontal">
+                <label class="label">
+                  To take away?
                     <input data-bind='toTakeAway' type="checkbox" style={"margin-left: 1em"} />
-                  </label>                 
-                  </div>
-                  <div class="field is-horizontal">
-                  <div class="field-label is-normal">
-                    <label class="label">Comments:</label>
-                  </div>
-                  <div class="field-body">
-                    <div class="field">
-                      <div class="control">
-                        <textarea data-bind="comments" class="textarea" placeholder="e.g send your cutest delivery boy"></textarea>
-                      </div>
+                </label>
+              </div>
+              <div class="field is-horizontal">
+                <div class="field-label is-normal">
+                  <label class="label">Comments:</label>
+                </div>
+                <div class="field-body">
+                  <div class="field">
+                    <div class="control">
+                      <textarea data-bind="comments" class="textarea" placeholder="e.g send your cutest delivery boy"></textarea>
                     </div>
                   </div>
                 </div>
-                    <button id="order-button" onClick={_ => this.submitForm()} class="button is-block is-info is-large is-fullwidth">Order Burrito</button>  
-                    <p id="order-message" class="help is-link has-text-weight-semibold"> {this.state.msg} </p>
-                    </div>
-                </div>
+              </div>
+              <button id="order-button" onClick={_ => this.submitForm()} class="button is-block is-info is-large is-fullwidth">Order Burrito</button>
+              <p id="order-message" class="help is-link has-text-weight-semibold"> {this.state.msg} </p>
+            </div>
+          </div>
         </FormGroup>
       </div>
     )
